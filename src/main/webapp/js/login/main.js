e2ad9c3ceadedcf514f5af9aefc57b605983b5bc
Jeffrey.Liu <906956368@qq.com>
2017-11-24 16:57:18 +0800

    登陆页优化

--- src/main/webapp/js/login/main.js
+++ src/main/webapp/js/login/main.js
@@ -1,22 +1,43 @@
 $(document).ready(function () {
+    /** button点击事件 **/
     $('.log-btn').click(function () {
-        var htmlobj = $.ajax({url: '/main/loginCheck',data:{userName:$('#UserName').val(),password:$('#Passwod').val()}, dataType: 'json', async: false});
-        var json = JSON.parse(htmlobj.responseText);
-        var msg = json.msg;
-        if(msg=='ok'){
-            window.location.href = '/main/index';
-        }else if(msg=='error_name'){
-            $('.alert').text('Invalid UserName');
-            $('.log-status').eq(0).addClass('wrong-entry');
-        }else{
-            $('.alert').text('Invalid Password');
-            $('.log-status').eq(1).addClass('wrong-entry');
-        }
-        $('.alert').fadeIn(500);
-        setTimeout("$('.alert').fadeOut(1500);", 3000);
+        getData();
     });
-    $('.form-control').keypress(function () {
+    $('.form-control').keypress(function (event) {
         $('.log-status').removeClass('wrong-entry');
+        if(event.keyCode == "13"){
+            getData();
+        }
     });
+
 });
+
+function getData() {
+    var userName = $('#UserName').val();
+    var password = $('#Passwod').val();
+    var msg = '';
+    if(userName && password){
+        var htmlobj = $.ajax({url: '/main/loginCheck',data:{userName:userName,password:password}, dataType: 'json', async: false});
+        var json = JSON.parse(htmlobj.responseText);
+        msg = json.msg;
+    }else{
+        if(!userName){
+            msg = 'error_name';
+        }else{
+            msg = 'error_password';
+        }
+    }
+
+    if(msg=='ok'){
+        window.location.href = '/main/index';
+    }else if(msg=='error_name'){
+        $('.alert').text('Invalid UserName');
+        $('.log-status').eq(0).addClass('wrong-entry');
+    }else{
+        $('.alert').text('Invalid Password');
+        $('.log-status').eq(1).addClass('wrong-entry');
+    }
+    $('.alert').fadeIn(500);
+    setTimeout("$('.alert').fadeOut(1500);", 3000);
+}
 
