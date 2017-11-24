817aab7e3ceda50b92572a574810a9b2899f1ba4
Jeffrey.Liu <906956368@qq.com>
2017-11-24 13:41:40 +0800

    增加登陆验证

--- src/main/java/org/jeffrey/controller/MainController.java
+++ src/main/java/org/jeffrey/controller/MainController.java
@@ -1,9 +1,13 @@
 package org.jeffrey.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import org.jeffrey.base.BaseMultiController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
 
 /**
  * 程序入口相关的controller
@@ -14,9 +18,35 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping(value = "/main")
 public class MainController extends BaseMultiController {
-    /** 登陆页 **/
+    /** 首页-登录页 **/
     @RequestMapping(value = "/login")
-    public ModelAndView test(){
+    public ModelAndView login(){
         return toView("/login/login",null);
+    }
+
+
+    /** 登陆 **/
+    @RequestMapping(value = "/signIn")
+    public ModelAndView signIn(@RequestParam("userName") String userName, @RequestParam("password") String password){
+        if("admin".equals(userName) && "1".equals(password)){
+            return toView("/main/index",null);
+        }else{
+            return toView("/login/login",null);
+        }
+    }
+
+    /** 登陆验证 **/
+    @ResponseBody
+    @RequestMapping(value = "/loginCheck",produces="text/html;charset=UTF-8")
+    public String loginCheck(@RequestParam("userName") String userName, @RequestParam("password") String password){
+        JSONObject jsonObject = new JSONObject();
+        if(!"admin".equals(userName)){
+            jsonObject.put("msg","用户名错误");
+        }else if(!"1".equals(password)){
+            jsonObject.put("msg","密码错误");
+        }else{
+            jsonObject.put("msg","ok");
+        }
+        return JSONObject.toJSONString(jsonObject);
     }
 }
--- src/main/webapp/js/login/main.js
+++ src/main/webapp/js/login/main.js
@@ -1,11 +1,20 @@
-  $(document).ready(function(){
-        $('.log-btn').click(function(){
-            $('.log-status').addClass('wrong-entry');
-           $('.alert').fadeIn(500);
-           setTimeout( "$('.alert').fadeOut(1500);",3000 );
-        });
-        $('.form-control').keypress(function(){
-            $('.log-status').removeClass('wrong-entry');
-        });
-
+$(document).ready(function () {
+    $('.log-btn').click(function () {
+        var htmlobj = $.ajax({url: '/main/loginCheck',data:{userName:$('#UserName').val(),password:$('#Passwod').val()}, dataType: 'json', async: false});
+        console.log(htmlobj.responseText);
+        var json = JSON.parse(htmlobj.responseText);
+        var msg = json.msg;
+        if(msg=='ok'){
+            console.log('ok');
+        }else{
+            console.log(msg);
+        }
+        $('.log-status').addClass('wrong-entry');
+        $('.alert').fadeIn(500);
+        setTimeout("$('.alert').fadeOut(1500);", 3000);
     });
+    $('.form-control').keypress(function () {
+        $('.log-status').removeClass('wrong-entry');
+    });
+});
+
